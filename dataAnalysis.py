eeea3fb62fce2967b4ec3a0009616842a977de0b
wccou <1281585844@qq.com>
2019-05-09 15:24:10 +0800

    数据分析：归一化最大/平均 tardiness/response time

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -11,35 +11,60 @@ import matplotlib.pyplot as plt
 用来数据分析画图
 '''
 if __name__ == '__main__':
-    a = np.loadtxt('m8alpha4.txt')
-    b = np.loadtxt('m8alpha2.txt')
+    a = np.loadtxt('./data/m16alpha4.txt')
+    b = np.loadtxt('./data/m16alpha3.txt')
+    c = np.loadtxt('./data/m16alpha2.txt')
     length1 = len(a)
     length2 = len(b)
-    x1 = []
-    y1 = []
+    length3 = len(c)
+    ax1 = []
+    ay1 = []
+    ay2 = []
+    ay3 = []
+    ay4 = []
     for i in range(length1):
-        x1.append(a[i][0])
-        y1.append(a[i][1])
-    print x1
-    print y1
+        ax1.append(a[i][0])
+        ay1.append(a[i][1])
+        ay2.append(a[i][2])
+        ay3.append(a[i][3])
+        ay4.append(a[i][4])
+    # print x1
+    # print y1
 
-    x2 = []
-    y2 = []
-    for i in range(length2):
-        x2.append(b[i][0])
-        y2.append(b[i][1])
-    print x2
-    print y2
+    # x2 = []
+    # y2 = []
+    # for i in range(length2):
+    #     x2.append(b[i][0])
+    #     y2.append(b[i][1])
+    # print x2
+    # print y2
 
-    plt.figure()
-    plt.plot(x1, y1, marker='.', mec='r', mfc='w',label='alpha=4')
-    plt.plot(x2, y2, marker='*', ms=10,label='alpha=2')
-    plt.legend()
+    plt.figure(figsize=(16, 12))
+    plt.subplot(221)
+    plt.plot(ax1, ay1, marker='*', ms=10)
     plt.xlabel("Total Utilization (Base)")
     plt.ylabel("Avg. of Normalized Max Tardiness")
-    plt.title("Normalized Tardiness by Total Util(m=8)")
+    plt.title("Normalized Tardiness by Total Util(m=16,alpha=4)")
+    plt.subplot(222)
+    plt.plot(ax1, ay2, marker='*', ms=10)
+    plt.xlabel("Total Utilization (Base)")
+    plt.ylabel("Avg. of Normalized Average Tardiness")
+    plt.title("Normalized Tardiness by Total Util(m=16,alpha=4)")
+    plt.subplot(223)
+    plt.plot(ax1, ay3, marker='*', ms=10)
+    plt.xlabel("Total Utilization (Base)")
+    plt.ylabel("Avg. of Normalized Max Response Time")
+    plt.title("Normalized Response Time by Total Util(m=16,alpha=4)")
+    plt.subplot(224)
+    plt.plot(ax1, ay4, marker='*', ms=10)
+    plt.xlabel("Total Utilization (Base)")
+    plt.ylabel("Avg. of Normalized Average Response Time")
+    plt.title("Normalized Response Time by Total Util(m=16,alpha=4)")
+
+    plt.legend()
+
     # plt.show()
-    plt.savefig("/Users/weichenchen/Desktop/实时系统/实验结果/m8.png", format="PNG")
+    #  plt.savefig("/Users/weichenchen/Desktop/实时系统/实验结果/m8.png", format="PNG")
     plt.show()
 
 
