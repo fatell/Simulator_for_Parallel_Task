f5ae1807f80b7d937d64784cb2dcc41a4cb3c790
wccou <1281585844@qq.com>
2019-05-09 16:56:32 +0800

    补充实验数据，使其更密

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -22,12 +22,33 @@ if __name__ == '__main__':
     ay2 = []
     ay3 = []
     ay4 = []
+    bx1 = []
+    by1 = []
+    by2 = []
+    by3 = []
+    by4 = []
+    cx1 = []
+    cy1 = []
+    cy2 = []
+    cy3 = []
+    cy4 = []
     for i in range(length1):
         ax1.append(a[i][0])
+        bx1.append(b[i][0])
+        cx1.append(c[i][0])
         ay1.append(a[i][1])
         ay2.append(a[i][2])
         ay3.append(a[i][3])
         ay4.append(a[i][4])
+        by1.append(b[i][1])
+        by2.append(b[i][2])
+        by3.append(b[i][3])
+        by4.append(b[i][4])
+        cy1.append(c[i][1])
+        cy2.append(c[i][2])
+        cy3.append(c[i][3])
+        cy4.append(c[i][4])
+
     # print x1
     # print y1
 
@@ -41,26 +62,40 @@ if __name__ == '__main__':
 
     plt.figure(figsize=(16, 12))
     plt.subplot(221)
-    plt.plot(ax1, ay1, marker='*', ms=10)
+    plt.plot(ax1, ay1, marker='*', ms=10, label='alpha=4')
+    plt.plot(bx1, by1, marker='o', ms=10, label='alpha=3')
+    plt.plot(cx1, cy1, marker='>', ms=10, label='alpha=2')
     plt.xlabel("Total Utilization (Base)")
     plt.ylabel("Avg. of Normalized Max Tardiness")
     plt.title("Normalized Tardiness by Total Util(m=16,alpha=4)")
+    plt.grid(True)
+    plt.legend()
     plt.subplot(222)
-    plt.plot(ax1, ay2, marker='*', ms=10)
+    plt.plot(ax1, ay2, marker='*', ms=10, label='alpha=4')
+    plt.plot(bx1, by2, marker='o', ms=10, label='alpha=3')
+    plt.plot(cx1, cy2, marker='<', ms=10, label='alpha=2')
     plt.xlabel("Total Utilization (Base)")
     plt.ylabel("Avg. of Normalized Average Tardiness")
     plt.title("Normalized Tardiness by Total Util(m=16,alpha=4)")
+    plt.grid(True)
+    plt.legend()
     plt.subplot(223)
-    plt.plot(ax1, ay3, marker='*', ms=10)
+    plt.plot(ax1, ay3, marker='*', ms=10, label='alpha=4')
+    plt.plot(bx1, by3, marker='o', ms=10, label='alpha=3')
+    plt.plot(cx1, cy3, marker='^', ms=10, label='alpha=2')
     plt.xlabel("Total Utilization (Base)")
     plt.ylabel("Avg. of Normalized Max Response Time")
     plt.title("Normalized Response Time by Total Util(m=16,alpha=4)")
+    plt.grid(True)
+    plt.legend()
     plt.subplot(224)
-    plt.plot(ax1, ay4, marker='*', ms=10)
+    plt.plot(ax1, ay4, marker='*', ms=10, label='alpha=4')
+    plt.plot(bx1, by4, marker='o', ms=10, label='alpha=3')
+    plt.plot(cx1, cy4, marker='v', ms=10, label='alpha=2')
     plt.xlabel("Total Utilization (Base)")
     plt.ylabel("Avg. of Normalized Average Response Time")
     plt.title("Normalized Response Time by Total Util(m=16,alpha=4)")
-
+    plt.grid(True)
     plt.legend()
 
     # plt.show()
