3aec2b7933648e581f57d2ee68aad482b497c89d
wccou <1281585844@qq.com>
2019-05-10 21:28:42 +0800

    数据分析改进

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -13,9 +13,11 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     a = np.loadtxt('./data/m16alpha4.txt')
     b = np.loadtxt('./data/m16alpha3.txt')
+    d = np.loadtxt('./data/FSm16alpha3.txt')
     c = np.loadtxt('./data/m16alpha2.txt')
     length1 = len(a)
     length2 = len(b)
+    length4 = len(d)
     length3 = len(c)
     ax1 = []
     ay1 = []
@@ -32,10 +34,16 @@ if __name__ == '__main__':
     cy2 = []
     cy3 = []
     cy4 = []
+    dx1 = []
+    dy1 = []
+    dy2 = []
+    dy3 = []
+    dy4 = []
     for i in range(length1):
         ax1.append(a[i][0])
         bx1.append(b[i][0])
         cx1.append(c[i][0])
+        dx1.append(d[i][0])
         ay1.append(a[i][1])
         ay2.append(a[i][2])
         ay3.append(a[i][3])
@@ -48,6 +56,10 @@ if __name__ == '__main__':
         cy2.append(c[i][2])
         cy3.append(c[i][3])
         cy4.append(c[i][4])
+        dy1.append(d[i][1])
+        dy2.append(d[i][2])
+        dy3.append(d[i][3])
+        dy4.append(d[i][4])
 
     # print x1
     # print y1
@@ -105,7 +117,8 @@ if __name__ == '__main__':
     plt.figure(figsize=(16, 12))
     plt.subplot(221)
     # plt.plot(ax1, ay1, marker='*', ms=10, label='alpha=4')
-    plt.plot(bx1, by1, marker='o', ms=10, label='alpha=3')
+    plt.plot(bx1, by1, marker='o', ms=10, label='GEDF')
+    plt.plot(dx1, dy1, marker='*', ms=10, label='FS')
     # plt.plot(cx1, cy1, marker='>', ms=10, label='alpha=2')
     plt.xlabel("Total Utilization (Base)")
     plt.ylabel("Avg. of Normalized Max Tardiness")
@@ -114,7 +127,8 @@ if __name__ == '__main__':
     plt.legend()
     plt.subplot(222)
     # plt.plot(ax1, ay2, marker='*', ms=10, label='alpha=4')
-    plt.plot(bx1, by2, marker='o', ms=10, label='alpha=3')
+    plt.plot(bx1, by2, marker='o', ms=10, label='GEDF')
+    plt.plot(dx1, dy2, marker='*', ms=10, label='FS')
     # plt.plot(cx1, cy2, marker='<', ms=10, label='alpha=2')
     plt.xlabel("Total Utilization (Base)")
     plt.ylabel("Avg. of Normalized Average Tardiness")
@@ -123,7 +137,8 @@ if __name__ == '__main__':
     plt.legend()
     plt.subplot(223)
     # plt.plot(ax1, ay3, marker='*', ms=10, label='alpha=4')
-    plt.plot(bx1, by3, marker='o', ms=10, label='alpha=3')
+    plt.plot(bx1, by3, marker='o', ms=10, label='GEDF')
+    plt.plot(dx1, dy3, marker='*', ms=10, label='FS')
     # plt.plot(cx1, cy3, marker='^', ms=10, label='alpha=2')
     plt.xlabel("Total Utilization (Base)")
     plt.ylabel("Avg. of Normalized Max Response Time")
@@ -132,7 +147,8 @@ if __name__ == '__main__':
     plt.legend()
     plt.subplot(224)
     # plt.plot(ax1, ay4, marker='*', ms=10, label='alpha=4')
-    plt.plot(bx1, by4, marker='o', ms=10, label='alpha=3')
+    plt.plot(bx1, by4, marker='o', ms=10, label='GEDF')
+    plt.plot(dx1, dy4, marker='*', ms=10, label='FS')
     # plt.plot(cx1, cy4, marker='v', ms=10, label='alpha=2')
     plt.xlabel("Total Utilization (Base)")
     plt.ylabel("Avg. of Normalized Average Response Time")
