3c2da933c6415dd571c01dd2c5ffabafe78085c6
wccou <1281585844@qq.com>
2019-05-21 10:31:47 +0800

    FS调度的关于低利用率任务的核心分配bug解决

--- scheduler.py
+++ scheduler.py
@@ -325,14 +325,17 @@ def gedf_scheduler_FS(taskset, corenum, time):
             b = a[0]
             cores_assign_of_high[b] = cores_assign_of_high[b] - 1
             n = n - 1
+    sum_of_high_cores = 0
     for task in high_tasks:
         temp_task_set = []
         temp_task_set.append(task)
         cores_num = int(cores_assign_of_high[task])
+        sum_of_high_cores = sum_of_high_cores + cores_num
         tem_high_result, high_finish_job, high_m1, high_m2 = gedf_scheduler(temp_task_set, cores_num, time)
         high_result.append(tem_high_result)
         finish_job.extend(high_finish_job)
 
+    cores_of_low = corenum - sum_of_high_cores
             # 将低利用率任务转换为非并行任务
     for task in low_tasks:
         task.size = 1
