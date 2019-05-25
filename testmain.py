9e678c89add6416616d25a7c7c9f790614c97ec6
wccou <1281585844@qq.com>
2019-05-25 11:17:13 +0800

    增加对修改版gedf调度器结果的分析

--- testmain.py
+++ testmain.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
     print "任务生成完毕！"
     analyse_list = []
     analyse_list_FS = []
+    analyse_list_GEDF_modify = []
     for i in range(NUM_OF_TASKSETS):
         period_list = []
         execution_cost = 0
@@ -49,12 +50,13 @@ if __name__ == '__main__':
         time = 10 * least_common_mutiple
         average_execution_cost = 1.0 * execution_cost / len(tasksets[i])
         average_deadline = 1.0 * sum_of_deadline / len(tasksets[i])
-
         result, finish_job, m1, m2 = gedf_scheduler(tasksets[i], CORENUM, time)
-
         analyse_result = analyse_finish_job(finish_job, average_deadline)
         high_result, low_result, FS_finish_job = gedf_scheduler_FS(tasksets[i], CORENUM, time)
         analyse_result_FS = analyse_finish_job(FS_finish_job, average_deadline)
+
+        GEDF_modify_result, GEDF_modify_finish_job = gedf_modify_scheduler(tasksets[i], CORENUM, time)
+        analyse_result_GEDF_modify = analyse_finish_job(GEDF_modify_finish_job, average_deadline)
         # 对finish_job进行数据处理分析，分别求归一化后的最大和平均tardiness，response_time
         # max_tardiness = finish_job[-1].tardiness
         # finish_job.sort(key=lambda item: item.response_time)
@@ -75,6 +77,7 @@ if __name__ == '__main__':
         # analyse_result = [max_normalized_tardiness, average_normalized_tardiness, max_normalized_response_time, average_normalized_response_time]
         analyse_list.append(analyse_result)
         analyse_list_FS.append(analyse_result_FS)
+        analyse_list_GEDF_modify.append(analyse_result_GEDF_modify)
         # for item in finish_job:
         #     print item.ID,":",item.tardiness
         # print "m1:",m1,"m2",m2
@@ -99,10 +102,30 @@ if __name__ == '__main__':
 
     print "平均max_normalized_tardiness, 平均average_normalized_tardiness," \
           "平均max_normalized_response_time,平均average_normalized_response_time:"
-    print average1
-    print average2
-    print average3
-    print average4
+    print average1, average2, average3, average4
+
+    print "GEDF_modify!!!!:"
+    sum_1 = 0
+    sum_2 = 0
+    sum_3 = 0
+    sum_4 = 0
+    for item in analyse_list_GEDF_modify:
+        print item[0], item[1], item[2], item[3]
+        sum_1 = sum_1 + item[0]
+        sum_2 = sum_2 + item[1]
+        sum_3 = sum_3 + item[2]
+        sum_4 = sum_4 + item[3]
+    average_1 = 1.0 * sum_1 / len(analyse_list_GEDF_modify)
+    average_2 = 1.0 * sum_2 / len(analyse_list_GEDF_modify)
+    average_3 = 1.0 * sum_3 / len(analyse_list_GEDF_modify)
+    average_4 = 1.0 * sum_4 / len(analyse_list_GEDF_modify)
+
+    print "平均max_normalized_tardiness, 平均average_normalized_tardiness," \
+          "平均max_normalized_response_time,平均average_normalized_response_time:"
+    print average_1, average_2, average_3, average_4
+
+
+
 
     print "FS!!!!!!:"
     sum5 = 0
@@ -122,8 +145,5 @@ if __name__ == '__main__':
 
     print "平均max_normalized_tardiness, 平均average_normalized_tardiness," \
           "平均max_normalized_response_time,平均average_normalized_response_time:"
-    print average5
-    print average6
-    print average7
-    print average8
+    print average5, average6, average7, average8
 
