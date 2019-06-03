168d755302733a051acb7028021ae3bf71f3db2e
wccou <1281585844@qq.com>
2019-06-03 15:15:30 +0800

    结果分析中对图的中文支持，调节子图间距

--- gamma.py
+++ gamma.py
@@ -10,15 +10,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as st
 #from taskSetsGenerator import *
+import matplotlib
 if __name__ == '__main__':
-    # fig=plt.figure(figsize=(18,6))#确定绘图区域尺寸
-    # ax1=fig.add_subplot(1,2,1)#将绘图区域分成左右两块
-    # ax2=fig.add_subplot(1,2,2)
-    # x=np.arange(0.01,15,0.01)#生成数列
-    #
-    #
-    # z3=st.gamma.pdf(x,2,scale=1)
-    a=b=c=[]
-    print a, b, c
+
+
+    a = sorted([f.name for f in matplotlib.font_manager.fontManager.ttflist])
+
+    for i in a:
+        print i
 
 
